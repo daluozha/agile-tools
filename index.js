c142ddf0d88f4d7c5c37cf10b7121c58dd1dd22a
daluozha <561890199@qq.com>
2021-03-17 18:02:48 +0800

    puppeteer startup

--- index.js
+++ index.js
@@ -39,7 +39,35 @@ let scrape = async () => {
 };
 
   
-scrape().then((value) => {
-    console.log(value); // 成功!
-});
-  
+// scrape().then((value) => {
+//     console.log(value); // 成功!
+// });
+
+
+// (async () => {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto('https://news.ycombinator.com', {waitUntil: 'networkidle2'});
+//     await page.pdf({path: 'hn.pdf', format: 'A4'});
+
+//     await browser.close();
+// })();
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.baidu.com');
+
+    // Get the "viewport" of the page, as reported by the page.
+    const dimensions = await page.evaluate(() => {
+        return {
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight,
+        deviceScaleFactor: window.devicePixelRatio
+        };
+    });
+
+    console.log('Dimensions:', dimensions);
+
+    await browser.close();
+})();
