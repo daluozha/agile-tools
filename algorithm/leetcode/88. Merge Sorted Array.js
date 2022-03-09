ed5d4a62095fc58c4385d5a5211e4b5448b9eb5d
daluozha <561890199@qq.com>
2022-03-09 20:59:20 +0800

    repo 'leetcode' revert

---
+++ algorithm/leetcode/88. Merge Sorted Array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+ var merge = function(nums1, m, nums2, n) {
+    let i = m - 1, j = n - 1
+    while(i >= 0 && j >= 0)
+        nums1[i + j + 1] = nums2[j] > nums1[i] ? nums2[j--] : nums1[i--]
+    while(j >= 0) 
+        nums1[j] = nums2[j--]
+    return nums1
+
+    // todo 指针，顺序 + 逆序
+};
