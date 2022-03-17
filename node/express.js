0ffb03de91f894f18f3b7a3d3b08ef899e76bc72
daluozha <561890199@qq.com>
2022-03-17 10:19:26 +0800

    express get post method

--- node/express.js
+++ node/express.js
@@ -1,3 +1,26 @@
 // express 与 http 在功能上有什么不一样的地方？
 
+const express = require('express')
+const app = express()
 
+// app.use((req, res) => {
+//     res.json({
+//         name: 'tom'
+//     })
+// })
+
+app.post('/name', (req, res) => {
+    let { age } = req.params;
+    res.send('tom')
+})
+
+app.get('/name/:age', (req, res) => {
+    let { age } = req.params;
+    res.json({
+        name: 'tom',
+        age
+    })
+})
+app.listen(3000, () => {
+    console.log('running on http://127.0.0.1:3000')
+})
