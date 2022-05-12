b681e556e7be151bf6c2a71acef585784d902ee6
daluozha <561890199@qq.com>
2022-05-12 18:36:46 +0800

    配置 css-loader

--- webpack-src/index.js
+++ webpack-src/index.js
@@ -1,2 +1,4 @@
+import './index.css'
+import './main.css'
 const hello = require('./hello.js')
 document.querySelector('#root').appendChild(hello())
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 // 有了这个配置文件，我们只需在终端中运行 npx webpack 命令就可进行打包，这条命令会自动引用 webpack.config.js 文件中的配置选项。
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
 const path = require("path");
-console.log(path.join(__dirname, "/webpack-src/index.js"));
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PurifyCssWebpack = require('purifycss-webpack') // 引入PurifyCssWebpack插件
+const glob = require('glob') // 引入glob模块,用于扫描全部html文件中所引用的css
+
+
+// console.log(path.join(__dirname, "/webpack-src/index.js"));
 module.exports = {
   mode: "development",
   entry: path.join(__dirname, "/webpack-src/index.js"),
   output: {
     path: path.join(__dirname, "/webpack-dist"),
     filename: "bundle.js",
-    clean: true
+    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -23,13 +29,45 @@ module.exports = {
       },
     }),
     new CleanWebpackPlugin(),
+    // new webpack.HotModuleReplacementPlugin(),
+    require("autoprefixer"),
+    new MiniCssExtractPlugin(),
+    // 报错
+    // new PurifyCssWebpack({
+    //   paths: glob.sync(path.join(__dirname, 'webpack-src/*.html')) // 同步扫描所有html文件中所引用的css
+    // })
   ],
   module: {
     rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
+          },
+          { loader: "css-loader" },
+          {
+            loader: "postcss-loader",
+            // options: {
+            //   postcssOptions: {
+            //     plugins: [
+            //       autoprefixer({
+            //         overrideBrowerslist: [
+            //           "last 10 Chrome versions",
+            //           "last 5 Firefox version",
+            //         ],
+            //       }),
+            //     ],
+            //   },
+            // },
+          },
+        ],
+      },
       // {
-      //   test: /\.css$/,
-      //   use: ['style-loader', 'css-loader']
-      // },{
       //   test: /\.(scss|sass)$/,
       //   use: ['style-loader', 'css-loader', 'sass-loader']
       // },{
@@ -52,5 +90,6 @@ module.exports = {
     port: "8088",
     // inline: true,
     historyApiFallback: true,
+    // hot: true
   },
 };
