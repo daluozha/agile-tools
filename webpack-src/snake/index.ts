df9ad6f7d79cc136498d08acb194a6967e9c2c0e
daluozha <561890199@qq.com>
2022-05-22 01:24:45 +0800

    将 class 拆分到 modules 文件夹

--- webpack-src/snake/index.ts
+++ webpack-src/snake/index.ts
@@ -1,30 +1,11 @@
 import "./style/index.less"
-
-class Food {
-    element: HTMLElement;
-    constructor() {
-        // food 一定不为空
-        this.element = document.getElementById('food')!
-
-    }
-
-    get X() {
-        return this.element.offsetLeft
-    }
-    get Y() {
-        return this.element.offsetTop
-    }
-    change() {
-
-        let top = Math.round(Math.random() * 29) * 10
-        let left = Math.round(Math.random() * 29) * 10
-        this.element.style.left = `${top}px`
-        this.element.style.top = `${left}px`
-    }
-}
-
+import Food from './modules/Food'
 const food = new Food()
-
-
 food.change()
-console.log(food.X, food.Y)
+
+
+// const scorePanel = new ScorePanel(100, 2)
+// for (let i = 0; i < 10; i++) {
+//     scorePanel.addScore()
+
+// }
