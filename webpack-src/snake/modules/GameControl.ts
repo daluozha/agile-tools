9d9a79b6fbab21fdbae6e5b1a1c0c10e4a0af5a8
daluozha <561890199@qq.com>
2022-05-22 14:20:13 +0800

    snake eat food

--- webpack-src/snake/modules/GameControl.ts
+++ webpack-src/snake/modules/GameControl.ts
@@ -6,6 +6,7 @@ class GameControl {
     food: Food
     scorePanel: ScorePanel
     direction: string = ''
+    isLive: boolean = true
     constructor() {
         this.snake = new Snake()
         this.food = new Food()
@@ -14,9 +15,57 @@ class GameControl {
     }
     init() {
         document.addEventListener('keydown', this.keydownHandler.bind(this))
+        this.run()
+
     }
     keydownHandler(event: KeyboardEvent) {
         this.direction = event.key
+    }
+    run() {
+        let X = this.snake.X
+        let Y = this.snake.Y
+        switch (this.direction) {
+            case "ArrowUp":
+            case "Up":
+                Y -= 10
+                break;
+            case "ArrowDown":
+            case "Down":
+                Y += 10
+                break;
+            case "ArrowLeft":
+            case "Left":
+                X -= 10
+                break;
+            case "ArrowRight":
+            case "Right":
+                X += 10
+                break;
+
+            default:
+                break;
+        }
+
+        this.checkEat(X, Y)
+            
+        
+
+        try {
+            this.snake.X = X
+            this.snake.Y = Y
+        } catch (error: any) {
+            alert(error.message)
+            this.isLive = false
+        }
+
+        this.isLive && setTimeout(this.run.bind(this), 300 - (this.scorePanel.level - 1) * 30);
+    }
+    checkEat(X: number, Y: number) {
+        if(X === this.food.X && Y === this.food.Y) {
+            this.food.change()
+            this.scorePanel.addScore()
+            this.snake.addBody()
+        }
     }
 }
 
--- webpack-src/snake/modules/Snake.ts
+++ webpack-src/snake/modules/Snake.ts
@@ -13,12 +13,25 @@ class Snake {
         return this.head.offsetLeft
     }
     set X(value: number) {
+        if (this.X === value) {
+            return
+        }
+        if (value < 0 || value > 290) {
+            throw new Error("game over");
+        }
         this.head.style.left = value + "px"
     }
     get Y() {
         return this.head.offsetTop
     }
     set Y(value: number) {
+        if (this.Y === value) {
+            return
+        }
+        if (value < 0 || value > 290) {
+            throw new Error("game over");
+
+        }
         this.head.style.top = value + "px"
     }
     addBody() {
