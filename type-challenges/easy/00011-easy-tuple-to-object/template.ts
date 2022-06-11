afd5977d86374c7ab8971340f2908192bc56416b
daluozha <561890199@qq.com>
2022-06-11 16:12:04 +0800

    00011-easy-tuple-to-object

--- type-challenges/easy/00011-easy-tuple-to-object/template.ts
+++ type-challenges/easy/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,7 @@
-type TupleToObject<T extends readonly any[]> = any
+// typeof
+// as const
+// 遍历数组
+
+type TupleToObject<T extends readonly (string | number | symbol)[]> = {
+    [P in T[number]]: P
+}
