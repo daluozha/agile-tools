ac9718591062f53bf881d303f04b404fcacde10e
daluozha <561890199@qq.com>
2022-06-11 22:35:21 +0800

    type-challenges easy

--- type-challenges/easy/00189-easy-awaited/template.ts
+++ type-challenges/easy/00189-easy-awaited/template.ts
@@ -3,8 +3,6 @@
 // 1.
 type MyAwaited<T extends Promise<any>> = T extends Promise<infer P> ? P extends Promise<any> ? MyAwaited<P> : P : never
 
-// type MyAwaited<T> = T extends Promise<infer P> ? P extends Promise<any> ? MyAwaited<P> : P : never
-
 
 // 2.
 // type MyReAwaited<T> = T extends Promise<infer P> ? MyReAwaited<P> : T
