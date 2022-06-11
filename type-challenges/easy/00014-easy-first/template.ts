5ea27046b4b08735e2125cddf76ec337e709b02d
daluozha <561890199@qq.com>
2022-06-11 16:35:56 +0800

    00014-easy-first

--- type-challenges/easy/00014-easy-first/template.ts
+++ type-challenges/easy/00014-easy-first/template.ts
@@ -1,1 +1,10 @@
-type First<T extends any[]> = any
+
+
+// 1.
+// type First<T extends any[]> = T extends [] ? never : T[0];
+
+// 2.
+// type First<T extends any[]> = T["length"] extends 0 ? never : T[0];
+
+// 3.
+type First<T extends any[]> = T[0] extends T[number] ? T[0] : never;
