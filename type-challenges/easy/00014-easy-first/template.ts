4cc488877a0c7554a190ca82326198be4b7d9cff
daluozha <561890199@qq.com>
2022-06-11 16:42:50 +0800

    00014-easy-first

--- type-challenges/easy/00014-easy-first/template.ts
+++ type-challenges/easy/00014-easy-first/template.ts
@@ -1,4 +1,4 @@
-
+// inter / infer
 
 // 1.
 // type First<T extends any[]> = T extends [] ? never : T[0];
@@ -7,4 +7,7 @@
 // type First<T extends any[]> = T["length"] extends 0 ? never : T[0];
 
 // 3.
-type First<T extends any[]> = T[0] extends T[number] ? T[0] : never;
+// type First<T extends any[]> = T[0] extends T[number] ? T[0] : never;
+
+// 4.
+type First<T extends any[]> = T extends [infer First, ... infer Rest] ? First : never;
