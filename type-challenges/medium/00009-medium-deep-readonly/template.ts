151d9bb8054eced79bffe15bbf0c2b2f09a45b5a
daluozha <561890199@qq.com>
2022-07-03 19:41:56 +0800

    deep readonly

--- type-challenges/medium/00009-medium-deep-readonly/template.ts
+++ type-challenges/medium/00009-medium-deep-readonly/template.ts
@@ -1,1 +1,12 @@
-type DeepReadonly<T> = any
+// type DeepReadonly<T> = {
+//     readonly [P in keyof T]: keyof T[P] extends never
+//         ? T[P]
+//         : DeepReadonly<T[P]>;
+// };
+
+// 2.
+type DeepReadonly<T> = keyof T extends never
+    ? T
+    : {
+          readonly [key in keyof T]: DeepReadonly<T[key]>;
+      };
