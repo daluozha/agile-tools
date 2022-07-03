dddca3ab8769f57bbe0841757910a60e9f888d40
daluozha <561890199@qq.com>
2022-07-03 19:34:01 +0800

    readonly2

--- type-challenges/medium/00008-medium-readonly-2/template.ts
+++ type-challenges/medium/00008-medium-readonly-2/template.ts
@@ -1,1 +1,9 @@
-type MyReadonly2<T, K> = any
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+    readonly [P in K]: T[P];
+} & MyOmit<T, K>;
+
+
+// 2. 这样写不行
+// type MyReadonly2<T, K extends keyof T = keyof T> = T & {
+//     readonly [P in K]: T[P];
+// };
