b6e145481b903e33e5955867adfd28f3eb74d3a6
daluozha <561890199@qq.com>
2022-07-04 21:24:34 +0800

    InlineSnapshot

--- jest/tests/generateConfig.test.ts
+++ jest/tests/generateConfig.test.ts
@@ -1,7 +1,22 @@
 import { generateConfig } from "../utils/generateConfig";
 
-test("test generateConfig", () => {
-    expect(generateConfig()).toMatchSnapshot({
-        time: expect.any(Date)
-    });
+// test("test generateConfig", () => {
+//     expect(generateConfig()).toMatchSnapshot({
+//         time: expect.any(Date)
+//     });
+// });
+test("test generateConfig2", () => {
+    expect(generateConfig()).toMatchInlineSnapshot(
+        {
+            time: expect.any(Date),
+        },
+        `
+Object {
+  "domain": "localhost",
+  "port": 8080,
+  "server": "http://localhost",
+  "time": Any<Date>,
+}
+`
+    );
 });
--- type-challenges/3-hard/00006-hard-simple-vue/template.ts
+++ type-challenges/3-hard/00006-hard-simple-vue/template.ts
@@ -1,1 +1,14 @@
-declare function SimpleVue(options: any): any
+// declare function SimpleVue(options: any): any
+declare function SimpleVue<T, U, K>(options: {
+    data: () => T
+    computed: U & ThisType<T>
+    methods: K &
+      ThisType<
+        {
+          [key in keyof U]: U[key] extends (...args: any[]) => unknown
+            ? ReturnType<U[key]>
+            : never
+        } & T &
+          K
+      >
+  }): any
