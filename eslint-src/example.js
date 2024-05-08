343c3d7ff1353e144ccbceea4edda6d06c3c77e4
yucheng514 <561890199@qq.com>
2024-05-08 20:23:30 +0800

    chore: format vue-origin.js

--- eslint-src/example.js
+++ eslint-src/example.js
@@ -1,5 +1,5 @@
 module.exports = function(){
-    let hello = document.createElement('div');
-    hello.innerHTML = "nihao111!"
-  return hello;
+    let hello = document.createElement("div");
+    hello.innerHTML = "nihao111!";
+    return hello;
 };
